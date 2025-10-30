e61fbcfd4d04cee9c5d280ee6b04f7f4ebf3c643
Areeba-Farhan1234 <areebakhan1454771@gmail.com>
2025-10-30 12:05:24 +0500

    LASTEST COMMIT

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Menu from "./components/Menu";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Footer from "./components/Reveal";
 
 function App() {
   return (
